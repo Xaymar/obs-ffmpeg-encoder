8b80dc57ad672ae515bf883384530db11e00fb61
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2019-07-21 02:39:34 +0200

    strings: Extra shared strings for states, ratecontrol and keyframes

--- source/strings.hpp
+++ source/strings.hpp
@@ -18,6 +18,11 @@
 #pragma once
 #include "utility.hpp"
 
+#define P_STATE_DEFAULT "State.Default"
+#define P_STATE_DISABLED "State.Disabled"
+#define P_STATE_ENABLED "State.Enabled"
+#define P_STATE_AUTOMATIC "State.Automatic"
+
 #define P_RATECONTROL "RateControl"
 #define P_RATECONTROL_METHOD "RateControl.Method"
 #define P_RATECONTROL_METHOD_(x) "RateControl.Method." D_VSTR(x)
@@ -25,3 +30,19 @@
 #define P_RATECONTROL_BITRATE_MINIMUM "RateControl.Bitrate.Minimum"
 #define P_RATECONTROL_BITRATE_MAXIMUM "RateControl.Bitrate.Maximum"
 #define P_RATECONTROL_BUFFERSIZE "RateControl.BufferSize"
+#define P_RATECONTROL_QUALITY_TARGET "RateControl.Quality.Target"
+#define P_RATECONTROL_QUALITY_MINIMUM "RateControl.Quality.Minimum"
+#define P_RATECONTROL_QUALITY_MAXIMUM "RateControl.Quality.Maximum"
+#define P_RATECONTROL_QP_I "RateControl.QP.I"
+#define P_RATECONTROL_QP_P "RateControl.QP.P"
+#define P_RATECONTROL_QP_B "RateControl.QP.B"
+#define P_RATECONTROL_QP_I_INITIAL "RateControl.QP.I.Initial"
+#define P_RATECONTROL_QP_P_INITIAL "RateControl.QP.P.Initial"
+#define P_RATECONTROL_QP_B_INITIAL "RateControl.QP.B.Initial"
+
+#define P_KEYFRAMES "KeyFrames"
+#define P_KEYFRAMES_INTERVALTYPE "KeyFrames.IntervalType"
+#define P_KEYFRAMES_INTERVALTYPE_(x) "KeyFrames.IntervalType." D_VSTR(x)
+#define P_KEYFRAMES_INTERVAL "KeyFrames.Interval"
+#define P_KEYFRAMES_INTERVAL_SECONDS "KeyFrames.Interval.Seconds"
+#define P_KEYFRAMES_INTERVAL_FRAMES "KeyFrames.Interval.Frames"
